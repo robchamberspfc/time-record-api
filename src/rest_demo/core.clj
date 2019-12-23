087627082de29ba1f5ea8568678cf1e9c4eb2b1e
robchamberspfc <robchamberspfc@gmail.com>
2019-12-23 20:59:04 +0000

    end of tutorial and first GET endpoint

--- src/rest_demo/core.clj
+++ src/rest_demo/core.clj
@@ -1,7 +1,40 @@
 (ns rest-demo.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
 
+; Simple Body Page
+(defn simple-body-page [req] ;(3)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Try '/times'"})
+
+;new
+(defn times-handler [req]
+   (def times (json/read-str (slurp "resources/test.json")))
+      {:status  200
+         :headers {"Content-Type" "text/json"}
+         :body (json/write-str times)}
+   )
+
+; Our main routes
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (GET "/times" [] times-handler)
+  (route/not-found "Error, page not found!"))
+
+; Our main entry function
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This is our main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
